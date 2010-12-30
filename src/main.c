ddcda3f3caf4e98e93ae177a50ec31195bcb0c2c
boredomist <erik.price16@gmail.com>
2010-12-30 14:44:31 -0500

    Added command line options, ability to run file directly instead of compiling to bytecode

--- src/main.c
+++ src/main.c
@@ -31,7 +31,11 @@ static int writer(AttoVM* vm, const void *p, size_t s, void *d) {
 
 static int usage() {
   fprintf(stderr, "Usage:\n"
-	  "frink [options] file\n");
+	  "frink [options] file\n"
+          "-v\tDisplay version and exit\n"
+          "-h\tDisplay this help text\n"
+          "-o file\tWrite Atto bytecode to file\n"
+          "-c\tDon't compile or run, just check syntax\n");
   return 1;
 }
 
@@ -41,9 +45,50 @@ int main(int argc, char** argv) {
     return usage();
   }
 
+  int check     = 0;
+  int out       = 0;
+  char* outfile = NULL;
+  char* file    = NULL;
 
-  FILE* fp = fopen(argv[1], "r");
-  char* name = argv[1];
+  int i;
+  for(i = 1; i < argc; ++i) {
+    char *arg = argv[i];
+    if(arg[0] == '-') {
+      switch(arg[1]) {
+      case 'v':
+	printf("frink v%d.%d, running on atto v%d.%d\n", FRINK_VERSION_MAJOR, 
+               FRINK_VERSION_MINOR, VERSION_MAJOR, VERSION_MINOR);
+	return 0;
+      case 'h':
+	usage();
+	return 0;
+      case 'o':
+        out = 1;
+        if(i < argc - 1) {
+          outfile = argv[++i];
+        } else { 
+          fprintf(stderr, "No output file given\n");
+          return usage();
+        }
+        break;
+      case 'c':
+        check = 1;
+        break;
+      default:
+	fprintf(stderr, "Unrecognized switch -%c\n", arg[1]);
+	return usage();
+      }
+    } else {
+      if(file != NULL) {
+	return usage();
+      }
+      file = arg;
+    }
+  }
+
+
+  FILE* fp = fopen(file, "r");
+  char* name = file;
 
   FrinkProgram *f = LoadFile(fp, name);
   AttoBlock* b = compileFrink(f);
@@ -55,17 +100,53 @@ int main(int argc, char** argv) {
     return EXIT_FAILURE;
   }
 
+  if(check) {
+    puts("Syntax OK.");
+    return EXIT_SUCCESS;
+  }
+
   AttoVM* vm = AttoVMNew();
 
-  Proto* p = Proto_from_block(vm, b);
-  fp = fopen("out.ato", "wb");
-  dump(vm, p, writer, fp);
+  int status;
 
-  fclose(fp);
+  if(out) {
+    Proto* p = Proto_from_block(vm, b);
+    fp = fopen(outfile, "wb");
+    dump(vm, p, writer, fp);
+    fclose(fp);
+    status = EXIT_SUCCESS;
+  } else {
+    /* this could probably be extracted to its own function, since
+     * it is necessary for vm_interpret to run correctly
+     */
+    if(b->sizev > 0 ) {
+      TValue *null = malloc(sizeof(TValue));
+      *null = createNull();
+      b->vars = malloc(sizeof(TValue) * b->sizev);
+      
+      int i;
+      for( i = 0; i < b->sizev; ++i) {
+        b->vars[i] = createVar(null);
+      }
+    } else {
+      b->vars = NULL;
+    }
+
+    Stack* argStack = StackNew();
+    
+    TValue ret = vm_interpret(vm, b, 0, 0, argStack);
+    if(ret.type == TYPE_ERROR) {
+      status = EXIT_FAILURE;
+    } else {
+      status = EXIT_SUCCESS;
+    }
+    
+    StackDestroy(argStack);
+  }
   AttoVMDestroy(vm);
   AttoBlockDestroy(b);
 
-  return 0;
+  return status;
 
 }
 
